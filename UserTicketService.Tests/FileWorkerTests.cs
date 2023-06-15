49ae5807afff500b45dcf672cfaf3a512c0ac377
zaharlol <kovtun.zahar12@mail.ru>
2023-06-15 15:17:02 +0300

    FileWorkerTests

---
+++ UserTicketService.Tests/FileWorkerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserTicketService.Tests
+{
+    [TestFixture]
+    public class FileWorkerTests
+    {
+        [Test]
+        public void FWT()
+        {
+            var FWR = new FileWorker();
+
+            PrivateObject po = new PrivateObject(FWR);
+            var pF = po.Invoke("GetSafeFilename", @"Raaxa____asdasdasd");
+            NUnit.Framework.Assert.AreEqual(pF, "Raaxa____asdasdasd");
+        }
+    }
+}
---
+++ UserTicketService/FileWorker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserTicketService
+{
+    public class FileWorker
+    {
+        private string GetSafeFilename(string filename)
+        {
+            return string.Join("_", filename.Split(Path.GetInvalidFileNameChars()));
+        }
+    }
+}
